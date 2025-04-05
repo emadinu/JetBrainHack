8fcdb4ac7ba375d936c992e6c2786125f61c7aa5
GitHub Action <action@github.com>
2025-04-05 09:18:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/emadinu/jetbrainhack/services/MyProjectService.kt
+++ src/main/kotlin/com/github/emadinu/jetbrainhack/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.emadinu.jetbrainhack.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.emadinu.jetbrainhack.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/emadinu/jetbrainhack/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/emadinu/jetbrainhack/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.emadinu.jetbrainhack.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.emadinu.jetbrainhack.MyBundle
+import com.github.emadinu.jetbrainhack.services.MyProjectService
 import javax.swing.JButton
 
 
